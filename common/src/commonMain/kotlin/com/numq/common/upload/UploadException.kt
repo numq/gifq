cac0ea6027c3a9e9b6298cb86c3dc65c6b741867
numq <numq.dev@gmail.com>
2023-04-27 03:57:10 +0300

    Provide upload exception changes

--- common/src/commonMain/kotlin/com/numq/common/upload/UploadException.kt
+++ common/src/commonMain/kotlin/com/numq/common/upload/UploadException.kt
@@ -1,5 +1,7 @@
 package com.numq.common.upload
 
 sealed class UploadException private constructor(override val message: String) : Exception(message) {
+    override fun toString() = message
+
     object InvalidFileSize : UploadException("The file size must not exceed 100 megabytes.")
 }
