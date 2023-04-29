e5fa5e89701f049a0adba6c750c71437c59bb5e0
numq <numq.dev@gmail.com>
2023-04-29 03:02:07 +0300

    Add expected collector

--- common/src/androidMain/kotlin/com/numq/common/collector/Collector.kt
+++ common/src/androidMain/kotlin/com/numq/common/collector/Collector.kt
@@ -1,14 +1,20 @@
 package com.numq.common.collector
 
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.runtime.collectAsState
+//import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
 actual object Collector {
+
+    // TODO: 4/27/2023 provide custom collectWithLifecycle implementation
+    
     @Composable
-    actual fun <T> collect(flow: StateFlow<T>): T = flow.collectAsStateWithLifecycle().value
+    actual fun <T> collect(flow: StateFlow<T>): T = flow.collectAsState().value
+//    actual fun <T> collect(flow: StateFlow<T>): T = flow.collectAsStateWithLifecycle().value
 
     @Composable
-    actual fun <T> collect(flow: Flow<T>, initial: T?): T? = flow.collectAsStateWithLifecycle(initial).value
+    actual fun <T> collect(flow: Flow<T>, initial: T?): T? = flow.collectAsState(initial).value
+//    actual fun <T> collect(flow: Flow<T>, initial: T?): T? = flow.collectAsStateWithLifecycle(initial).value
 }
