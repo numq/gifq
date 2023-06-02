a9371e4ba8931264d682844c818987b0e27ddccf
numq <numq.dev@gmail.com>
2023-06-02 13:28:15 +0300

    Provide size formatter changes

--- common/src/commonMain/kotlin/com/numq/common/format/SizeFormatter.kt
+++ common/src/commonMain/kotlin/com/numq/common/format/SizeFormatter.kt
@@ -1,14 +1,13 @@
 package com.numq.common.format
 
+import kotlin.math.ceil
+
 object SizeFormatter {
-    fun format(size: Long): String {
-        val units = arrayOf("B", "KB", "MB", "GB", "TB")
-        var fileSize = size.toDouble()
-        var unitIndex = 0
-        while (fileSize > 1024 && unitIndex < units.size - 1) {
-            fileSize /= 1024
-            unitIndex++
-        }
-        return String.format("%.2f %s", fileSize, units[unitIndex])
+    fun format(sizeInBytes: Long): String {
+        val kb = ceil(sizeInBytes.toDouble() / 1024) to "KB"
+        val mb = ceil(sizeInBytes.toDouble() / (1024 * 1024)) to "MB"
+        return arrayOf(kb, mb).maxByOrNull { it.second }?.run {
+            String.format("%.2f %s", first, second)
+        } ?: "Unknown"
     }
 }
